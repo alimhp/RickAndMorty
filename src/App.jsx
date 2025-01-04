8399277406a1ebdfce86737d8f572b71b38e16ca
alimhp <alimohamadipourr@gmail.com>
2025-01-04 23:21:34 +0330

    add_loading_comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import "./App.css";
 import CharList from "./Component/CharList";
 import Navbar from "./Component/Navbar";
-import { allCharacters, character, episodes } from "../data/data";
+import { character, episodes } from "../data/data";
 import CharMain from "./Component/CharMain";
 import { useEffect, useState } from "react";
+import Loading from "./Component/Loading";
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
   // useEffect(() => {
+  //   setIsloading(true);
   //   fetch("https://rickandmortyapi.com/api/character")
   //     .then((res) => res.json())
-  //     .then((data) => setCharacters(data.results.slice(0,5)));
+  //     .then((data) => setCharacters(data.results.slice(0, 5)));
+  //   setIsloading(false);
   // }, []);
+  /////////////////////////////////////////////
   useEffect(() => {
     async function fetchdata() {
-      const data = await fetch("https://rickandmortyapi.com/api/character");
-      const res = await data.json();
-      setCharacters(res.results);
+      try {
+        setIsloading(true);
+        const data = await fetch("https://rickandmortyapi.com/api/character");
+        const res = await data.json();
+        setCharacters(res.results.slice(0, 5));
+        setIsloading(false);
+      } catch {
+        console.log(res.err);
+      }
     }
     fetchdata();
   }, []);
@@ -26,7 +37,7 @@ function App() {
         <Navbar numofchar={characters.length} />
       </div>
       <div className="mid">
-        <CharList characters={characters} />
+        {isLoading ? <Loading /> : <CharList characters={characters} />}
         <CharMain character={character} episodes={episodes} />
       </div>
     </div>
---
+++ src/Component/Loading.jsx
@@ -0,0 +1,6 @@
+import "../App.css"
+const Loading = () => {
+  return <div className="Loading">Loading....</div>;
+};
+
+export default Loading;
